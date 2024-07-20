5fc3fa7a608fbe53af15af61f8f0dd25c705d774
Vladyslav Pyrch <vlad.nuron@gmail.com>
2024-07-20 18:53:41 +0200

    Json body bug fix

---
+++ Ishtar/Middlewares/NoEndpointMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Ishtar.Abstractions;
+
+namespace Ishtar.Middlewares;
+
+
+public class NoEndpointMiddleware : IMiddleware
+{
+    public IMiddleware Next { get; set; } = null!;
+
+    public Task Invoke(IHttpContext context)
+    {
+        context.Response.StatusCode = context.Request.Method is { Value: "GET" or "HEAD" }
+            ? HttpStatusCode.Ok200 : HttpStatusCode.NotFound404;
+        return Task.CompletedTask;
+    }
+}
